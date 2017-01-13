398581801d3565db0c301b5d297d2466106b559c
MechaelaYun <f2010pyylye@163.com>
2017-01-13 09:38:14 +0800

    iputil project, to compute ip segments
    Signed-off-by: MechaelaYun <f2010pyylye@163.com>

---
+++ iputil/src/IPUtil/Nets.java
@@ -0,0 +1,25 @@
+package IPUtil;
+
+public class Nets {
+	private String StartIP;
+    private String EndIP;
+    private String NetMask;
+	public String getStartIP() {
+		return StartIP;
+	}
+	public void setStartIP(String startIP) {
+		StartIP = startIP;
+	}
+	public String getEndIP() {
+		return EndIP;
+	}
+	public void setEndIP(String endIP) {
+		EndIP = endIP;
+	}
+	public String getNetMask() {
+		return NetMask;
+	}
+	public void setNetMask(String netMask) {
+		NetMask = netMask;
+	}
+}
